f664745d2d7d75f98115b8e01501680fb505a6f8
16152939 <16152939@localhost.localdomain>
2014-10-15 14:58:04 +0800

    Created About Box

--- src/hci400assignment/ApplicationCore.java
+++ src/hci400assignment/ApplicationCore.java
@@ -49,11 +49,40 @@ public class ApplicationCore {
         rootFrame.repaint();
     }
 
+     public void showAboutBox()
+    {
+        rootFrame = new JFrame("MP400 Assignment");
+        rootFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+        rootPanel = new AboutBox();
+        rootFrame.getContentPane().add(rootPanel);
+
+        rootFrame.pack();
+
+        Dimension screenSize = Toolkit.getDefaultToolkit().getScreenSize();
+        Dimension frameSize = new Dimension(
+          (int)(screenSize.getWidth() * 3.0 / 5.0),
+          (int)(screenSize.getHeight() * 3.0 / 5.0)
+        );
+        rootFrame.setPreferredSize(frameSize);
+        rootFrame.setSize(frameSize);
+        rootFrame.setVisible(true);
+
+        rootFrame.setLocation(
+          screenSize.width / 2 - rootFrame.getSize().width / 2,
+          screenSize.height / 2 - rootFrame.getSize().height / 2
+        );
+
+        rootFrame.toFront();
+        rootFrame.repaint();
+    }
+
     /**
      * Main method launching the application.
      */
     public static void main(String[] args) {
         ApplicationCore app = new ApplicationCore();
-        app.launch();
+//        app.launch();
+        app.showAboutBox();
     }
 }
