c0770184976904117f76365f5e10d32df85317d3
Rhys Davies <rhysdavies1994@gmail.com>
2014-10-18 14:59:53 +0800

    White borders for buttons

--- src/hci400assignment/gui/StylePainter.java
+++ src/hci400assignment/gui/StylePainter.java
@@ -54,7 +54,7 @@ public class StylePainter
             );
 
             Graphics2D g2d = (Graphics2D)g;
-            g2d.setColor(Color.RED);
+            g2d.setColor(Color.WHITE);
             g2d.draw(rect);
         }
     }
