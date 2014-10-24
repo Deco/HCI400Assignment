5fcaa021bed6f0f752a5ffd3bd049305e2a8a614
Declan Wesley White <deco.da.man@gmail.com>
2014-10-24 17:42:53 +0800

    Removed uneeded code

--- src/hci400assignment/gui/StylePainter.java
+++ src/hci400assignment/gui/StylePainter.java
@@ -84,52 +84,6 @@ public class StylePainter
         paintVerticalGradient(context, graphics, x, y, w, h);
     }
 
-    @Override
-    public void paintCheckBoxBackground(SynthContext context, Graphics g, int x,
-      int y, int w, int h)
-    {
-
-        AbstractButton c = (AbstractButton)context.getComponent();
-        String text = c.getText();
-
-        Icon icon = c.getIcon();
-        if(icon == null) {
-            icon = context.getStyle().getIcon(context, "CheckBox.icon");
-        };
-        int iconTextGap = c.getIconTextGap();
-
-        Rectangle componentRect = new Rectangle();
-        Rectangle iconR = new Rectangle();
-
-        Insets insets = new Insets(0, 0, 0, 0);
-        if(context.getRegion().isSubregion()) {
-            insets = context.getStyle().getInsets(context, insets);
-        } else {
-            insets = context.getComponent().getInsets(insets);
-        }
-
-        componentRect.x = insets.left;
-        componentRect.y = insets.top;
-        componentRect.width = c.getWidth() - (insets.left + insets.right);
-        componentRect.height = c.getHeight() - (insets.top + insets.bottom);
-
-        if(icon != null) {
-            iconR.x += componentRect.x;
-            iconR.y += componentRect.y;
-            iconR.width = icon.getIconWidth();
-            iconR.height = icon.getIconHeight();
-
-            g.setColor(Color.GREEN);
-            g.fillRect(iconR.x, iconR.y, iconR.width, iconR.height);
-        }
-        if(text != null) {
-            g.setColor(Color.RED);
-            int textPos = iconR.x + iconR.width + iconTextGap;
-            g.fillRect(textPos, iconR.y, c.getWidth() - insets.right - textPos,
-              componentRect.height);
-        }
-    }
-
 
     //****** TEXTFIELD ******/
     // <editor-fold defaultstate="collapsed" desc="TextField Painters">
